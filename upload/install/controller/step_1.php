7feac7c62a37aa4b0e1ae24c0c52cd5aa431a52c
ocshop <info@vilkov.info>
2014-08-29 03:39:37 +0300

    Устранена ошибка при установке
    ошибка на первом шаге установки

--- upload/install/controller/step_1.php
+++ upload/install/controller/step_1.php
@@ -3,6 +3,12 @@ class ControllerStep1 extends Controller {
 	public function index() {
 		if ($this->request->server['REQUEST_METHOD'] == 'POST') {
 			$this->redirect($this->url->link('step_2'));
+		}
+
+		if (isset($this->error['warning'])) {
+			$this->data['error_warning'] = $this->error['warning'];
+		} else {
+			$this->data['error_warning'] = '';	
 		}	
 		
 		$this->data['action'] = $this->url->link('step_1');
