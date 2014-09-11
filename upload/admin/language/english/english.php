a6cdd4c51bd3fcb85a49a733396b0ea041468784
ocshop <info@vilkov.info>
2014-09-11 03:57:41 +0300

    Формат даты добавлено время часы и минуты

--- upload/admin/language/english/english.php
+++ upload/admin/language/english/english.php
@@ -2,7 +2,7 @@
 // Locale
 $_['code']                          = 'en';
 $_['direction']                     = 'ltr';
-$_['date_format_short']             = 'd/m/Y';
+$_['date_format_short']             = 'j.m.Y G:i';
 $_['date_format_long']              = 'l dS F Y';
 $_['time_format']                   = 'h:i:s A';
 $_['decimal_point']                 = '.';
