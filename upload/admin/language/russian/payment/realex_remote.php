ea4127223e930eff1adf74c48cff6127b9cc5e40
ocshop <info@vilkov.info>
2014-10-14 08:29:06 +0300

    fix локализация

--- upload/admin/language/russian/payment/realex_remote.php
+++ upload/admin/language/russian/payment/realex_remote.php
@@ -10,7 +10,7 @@ $_['text_card_type']				= 'Card type';
 $_['text_enabled']					= 'Enabled';
 $_['text_use_default']				= 'Use default';
 $_['text_merchant_id']				= 'Merchant ID';
-$_['text_subaccount']				= 'Subaccount';
+$_['text_subaccount']				= 'Sub Account';
 $_['text_secret']					= 'Shared secret';
 $_['text_card_visa']				= 'Visa';
 $_['text_card_master']				= 'Mastercard';
@@ -38,7 +38,7 @@ $_['text_transactions']				= 'Transactions';
 $_['text_confirm_void']				= 'Are you sure you want to void the payment?';
 $_['text_confirm_capture']			= 'Are you sure you want to capture the payment?';
 $_['text_confirm_rebate']			= 'Are you sure you want to rebate the payment?';
-$_['text_realex_remote']			= '<img src="view/image/payment/realex.png" alt="Realex" title="Realex" style="border: 1px solid #EEEEEE;" />';
+$_['text_realex_remote']			= '<a target="_BLANK" href="http://www.realexpayments.co.uk/partner-refer?id=opencart"><img src="view/image/payment/realex.png" alt="Realex" title="Realex" style="border: 1px solid #EEEEEE;" /></a>';
 
 // Column
 $_['text_column_amount']			= 'Amount';
@@ -57,13 +57,13 @@ $_['entry_debug']					= 'Debug logging';
 $_['entry_auto_settle']				= 'Settlement type';
 $_['entry_tss_check']				= 'TSS checks';
 $_['entry_card_data_status']		= 'Card info logging';
-$_['entry_3d']						= 'Enable 3D secure form form form form';
-$_['entry_liability_shift']			= 'Accept liability shift';
+$_['entry_3d']						= 'Enable 3D secure';
+$_['entry_liability_shift']			= 'Accept non-liability shifting scenarios';
 $_['entry_status_success_settled']	= 'Success - settled';
 $_['entry_status_success_unsettled'] = 'Success - not settled';
 $_['entry_status_decline']			= 'Decline';
 $_['entry_status_decline_pending']	= 'Decline - offline auth';
-$_['entry_status_decline_stolen']	= 'Decline - stolen card';
+$_['entry_status_decline_stolen']	= 'Decline - lost or stolen card';
 $_['entry_status_decline_bank']		= 'Decline - bank error';
 $_['entry_status_void']				= 'Voided';
 $_['entry_status_rebate']			= 'Rebated';
@@ -77,10 +77,10 @@ $_['help_liability']				= 'Accepting liability means you will still accept payme
 $_['help_card_data_status']			= 'Logs last 4 cards digits, expire, name, type and issuing bank information';
 
 // Tab
-$_['tab_account']					= 'API info';
-$_['tab_sub_account']				= 'Accounts';
-$_['tab_order_status']				= 'Order status';
-$_['tab_payment']					= 'Payment settings';
+$_['tab_api']					    = 'API Details';
+$_['tab_account']				    = 'Accounts';
+$_['tab_order_status']				= 'Order Status';
+$_['tab_payment']					= 'Payment Settings';
 
 // Button
 $_['button_capture']				= 'Capture';
@@ -90,4 +90,3 @@ $_['button_void']					= 'Void';
 // Error
 $_['error_merchant_id']				= 'Merchant ID is required';
 $_['error_secret']					= 'Shared secret is required';
-
--- upload/catalog/language/russian/payment/sagepay_direct.php
+++ upload/catalog/language/russian/payment/sagepay_direct.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_title']				= 'Credit Card / Debit Card (SagePay)';
-$_['text_credit_card']			= 'Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Credit Card Details';
+$_['text_credit_card']			= 'Card Details';
 $_['text_card_type']			= 'Card Type: ';
 $_['text_card_name']			= 'Card Name: ';
 $_['text_card_digits']			= 'Last Digits: ';
@@ -15,13 +15,13 @@ $_['entry_card']				= 'New or Existing Card: ';
 $_['entry_card_existing']		= 'Existing';
 $_['entry_card_new']			= 'New';
 $_['entry_card_save']			= 'Remember Card Details';
-$_['entry_cc_owner']			= 'Card Owner::::::::::::::::::::::::::::::::';
-$_['entry_cc_type']				= 'Card Type::::::::::::::::::::::::::::::::';
-$_['entry_cc_number']			= 'Card Number::::::::::::::::::::::::::::::::';
-$_['entry_cc_start_date']		= 'Card Valid From Date::::::::::::::::::::::::::::::::';
-$_['entry_cc_expire_date']		= 'Card Expiry Date::::::::::::::::::::::::::::::::';
-$_['entry_cc_cvv2']				= 'Card Security Code (CVV2)::::::::::::::::::::::::::::::::';
-$_['entry_cc_issue']			= 'Card Issue Number::::::::::::::::::::::::::::::::';
+$_['entry_cc_owner']			= 'Card Owner';
+$_['entry_cc_type']				= 'Card Type';
+$_['entry_cc_number']			= 'Card Number';
+$_['entry_cc_start_date']		= 'Card Valid From Date';
+$_['entry_cc_expire_date']		= 'Card Expiry Date';
+$_['entry_cc_cvv2']				= 'Card Security Code (CVV2)';
+$_['entry_cc_issue']			= 'Card Issue Number';
 $_['entry_cc_choice']			= 'Choose an Existing Card';
 
 // Help
