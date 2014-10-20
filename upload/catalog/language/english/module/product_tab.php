35fb0257807ff3898180ddccb557d9c0d44a6e5a
ocshop <info@vilkov.info>
2014-10-20 08:26:11 +0300

    Добавлен модуль Табы Товаров

---
+++ upload/catalog/language/english/module/product_tab.php
@@ -0,0 +1,13 @@
+<?php
+// Heading 
+$_['heading_title'] 	= 'Our Products';
+
+// Tab
+$_['tab_latest'] 		= 'Latest';
+$_['tab_bestseller'] 	= 'Bestseller';
+$_['tab_featured'] 		= 'Featured';
+$_['tab_special'] 		= 'Special';
+
+// Text
+$_['text_reviews']  	= 'Based on %s reviews.';
+$_['text_tax']      = 'Ex Tax:';
