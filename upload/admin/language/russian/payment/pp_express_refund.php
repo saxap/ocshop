da0394604b4dc6d20515c2a3bc785fefe34eec9f
ocshop <info@vilkov.info>
2015-04-07 15:17:09 +0300

    корректировка перевода

--- upload/admin/language/russian/payment/pp_express_refund.php
+++ upload/admin/language/russian/payment/pp_express_refund.php
@@ -5,6 +5,7 @@ $_['heading_title']					= 'Refund Transaction';
 // Text
 $_['text_pp_express']				= 'PayPal Express Checkout';
 $_['text_current_refunds']			= 'Refunds have already been done for this transaction. The max refund is';
+$_['text_refund']					= 'Refund';
 
 // Entry
 $_['entry_transaction_id']			= 'Transaction ID';
@@ -18,4 +19,3 @@ $_['button_refund']					= 'Issue refund';
 // Error
 $_['error_partial_amt']				= 'You must enter a partial refund amount';
 $_['error_data']					= 'Data missing from request';
-
