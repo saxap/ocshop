53f6b8eca224de2b53e1c7f00f5b5a17a67d7202
ocshop <info@vilkov.info>
2015-07-15 13:59:11 +0300

    Category4Level

---
+++ upload/admin/language/english/module/category4level.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Category 4 Level';
+
+// Text
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified category 4 level module!';
+$_['text_edit']        = 'Edit Category 4 Level Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify category 4 level module!';
