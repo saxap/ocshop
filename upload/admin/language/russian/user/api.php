305f0400c4ae3f4e40a5a3f0e00200bb06fa3906
ocshop <info@vilkov.info>
2015-08-03 03:24:27 +0300

    Дополнение перевода

--- upload/admin/language/russian/user/api.php
+++ upload/admin/language/russian/user/api.php
@@ -10,7 +10,7 @@ $_['text_edit']       	   = 'Редактирование';
 $_['text_ip']              = 'Ниже вы можете создать список IP\'s которым разрешон доступ к API. Ваш IP %s';
 
 // Column
-$_['column_username']      = 'Имя';
+$_['column_name']          = 'Название API';
 $_['column_status']        = 'Статус';
 $_['column_date_added']    = 'Дата добавления';
 $_['column_date_modified'] = 'Дата изменения';
@@ -19,12 +19,12 @@ $_['column_ip']            = 'IP';
 $_['column_action']        = 'Действие';
 
 // Entry
-$_['entry_username']       = 'Имя';
-$_['entry_password']       = 'Пароль';
+$_['entry_name']           = 'Название API';
+$_['entry_key']            = 'Ключ API';
 $_['entry_status']         = 'Статус';
 $_['entry_ip']             = 'IP';
 
 // Error
 $_['error_permission']     = 'У Вас нет прав для управления модулем !';
-$_['error_username']       = 'Имя должно быть от 3 до 20 символов!';
-$_['error_password']       = 'Пароль должен быть от 3 до 256 символов!';
+$_['error_name']       	   = 'Название API должно содержать от 4 до 20 символов!';
+$_['error_key']       	   = 'Ключ API должен содержать от 3 до 256 символов!';
