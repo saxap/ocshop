629536b0707d1ac86b2e04bd62ba13ebe248a0c6
ocshop <info@vilkov.info>
2015-08-10 15:53:54 +0300

    Добавил русский перевод admin

---
+++ upload/admin/language/russian/openbay/etsy.php
@@ -0,0 +1,24 @@
+<?php
+// *	@copyright	OCSHOP.CMS \ ocshop.net 2011 - 2015.
+// *	@demo	http://ocshop.net
+// *	@blog	http://ocshop.info
+// *	@forum	http://forum.ocshop.info
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+// Heading
+$_['heading_title']         		= 'Etsy';
+$_['text_openbay']					= 'OpenBay Pro';
+$_['text_dashboard']				= 'Etsy Dashboard';
+
+// Messages
+$_['text_success']         			= 'Настройки успешно изменены!';
+$_['text_heading_settings']         = 'Настройки';
+$_['text_heading_sync']             = 'Syncronise';
+$_['text_heading_register']         = 'Register here';
+$_['text_heading_products']        	= 'Item links';
+$_['text_heading_listings']        	= 'Etsy listings';
+
+// Errors
+$_['error_generic_fail']			= 'An unknown error just happened!';
+$_['error_permission']				= 'У вас недостаточно прав для внесения изменений!';
---
+++ upload/admin/language/russian/payment/securetrading_pp.php
@@ -0,0 +1,89 @@
+<?php
+// *	@copyright	OCSHOP.CMS \ ocshop.net 2011 - 2015.
+// *	@demo	http://ocshop.net
+// *	@blog	http://ocshop.info
+// *	@forum	http://forum.ocshop.info
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+$_['heading_title'] = 'Secure Trading Payment Pages';
+
+$_['text_securetrading_pp'] = '<a href="http://www.securetradingfs.com/partner/open-cart/" target="_blank"><img src="view/image/payment/secure_trading.png" alt="Secure Trading" title="Secure Trading" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_payment'] = 'Платеж';
+$_['text_all_geo_zones'] = 'All Geo Zones';
+$_['text_process_immediately'] = 'Process immediately';
+$_['text_wait_x_days'] = 'Wait %d days';
+$_['text_success'] = 'Настройки успешно изменены!';
+$_['text_pending_settlement'] = 'Pending Settlement';
+$_['text_authorisation_reversed'] = 'Authorisation was successfully reversed';
+$_['text_refund_issued'] = 'Refund was successfully issued';
+$_['text_pending_settlement_manually_overriden'] = 'Pending Settlement, manually overriden';
+$_['text_pending_suspended'] = 'Заморожен';
+$_['text_pending_settled'] = 'Settled';
+
+$_['entry_site_reference'] = 'Site reference';
+$_['entry_username'] = 'Username';
+$_['entry_password'] = 'Пароль';
+$_['entry_site_security_status'] = 'Use Site Security hash';
+$_['entry_site_security_password'] = 'Site Security password';
+$_['entry_notification_password'] = 'Notification password';
+$_['entry_order_status'] = 'Статус';
+$_['entry_declined_order_status'] = 'Declined order status';
+$_['entry_refunded_order_status'] = 'Refunded order status';
+$_['entry_authorisation_reversed_order_status'] = 'Authorisation reversed order status';
+$_['entry_settle_status'] = 'Settlement status';
+$_['entry_settle_due_date'] = 'Settlement due date';
+$_['entry_geo_zone'] = 'Географическая зона';
+$_['entry_sort_order'] = 'Порядок сортировки';
+$_['entry_status'] = 'Статус';
+$_['entry_total'] = 'Всего';
+$_['entry_parent_css'] = 'Parent CSS';
+$_['entry_child_css'] = 'Child CSS';
+$_['entry_cards_accepted'] = 'Accepted cards';
+$_['entry_reverse_authorisation'] = 'Reverse Authorisation:';
+$_['entry_refunded'] = 'Refunded:';
+$_['entry_refund'] = 'Issue refund (%s):';
+
+$_['error_permission'] = 'У вас недостаточно прав для внесения изменений!';
+$_['error_site_reference'] = 'Site Reference is required';
+$_['error_notification_password'] = 'Notification password is required';
+$_['error_cards_accepted'] = 'Accepted cards is required';
+$_['error_username'] = 'Username is required';
+$_['error_password'] = 'Password is required';
+$_['error_connection'] = 'Could not connect to Secure Trading';
+$_['error_data_missing'] = 'Data missing';
+
+$_['help_username'] = 'Your webservice username';
+$_['help_password'] = 'Your webservice password';
+$_['help_refund'] = 'Please include the decimal point and the decimal part of the amount';
+$_['help_total'] = 'The checkout total the order must reach before this payment method becomes active';
+
+$_['button_reverse_authorisation'] = 'Reverse Authorisation';
+$_['button_refund'] = 'Refund';
+
+
+// Order page - payment tab
+$_['text_payment_info'] = 'Платёжные реквизиты';
+$_['text_release_status'] = 'Payment released';
+$_['text_void_status'] = 'Reverse Authorisation';
+$_['text_rebate_status'] = 'Payment rebated';
+$_['text_order_ref'] = 'Order ref';
+$_['text_order_total'] = 'Total authorised';
+$_['text_total_released'] = 'Total released';
+$_['text_transactions'] = 'Операции';
+$_['text_column_amount'] = 'Итого';
+$_['text_column_type'] = 'Тип';
+$_['text_column_created'] = 'Создан';
+$_['text_release_ok'] = 'Release was successful';
+$_['text_release_ok_order'] = 'Release was successful, order status updated to success - settled';
+$_['text_rebate_ok'] = 'Rebate was successful';
+$_['text_rebate_ok_order'] = 'Rebate was successful, order status updated to rebated';
+$_['text_void_ok'] = 'Void was successful, order status updated to voided';
+
+$_['text_confirm_void'] = 'Are you sure you want to reverse the authorisation?';
+$_['text_confirm_release'] = 'Are you sure you want to release the payment?';
+$_['text_confirm_rebate'] = 'Are you sure you want to rebate the payment?';
+
+$_['btn_release'] = 'Release';
+$_['btn_rebate'] = 'Rebate / refund';
+$_['btn_void'] = 'Reverse Authorisation';
