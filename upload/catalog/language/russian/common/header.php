45d874d8463f4777d08b96a8ab834a4096ffbd13
ocshop <info@vilkov.info>
2015-08-10 15:56:16 +0300

    Добавил русский перевод catalog

---
+++ upload/catalog/language/russian/common/header.php
@@ -0,0 +1,23 @@
+<?php
+// *	@copyright	OCSHOP.CMS \ ocshop.net 2011 - 2015.
+// *	@demo	http://ocshop.net
+// *	@blog	http://ocshop.info
+// *	@forum	http://forum.ocshop.info
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+// Text
+$_['text_home']          = 'Главная';
+$_['text_wishlist']      = 'Закладки (%s)';
+$_['text_shopping_cart'] = 'Корзина';
+$_['text_category']      = 'Категории';
+$_['text_account']       = 'Личный кабинет';
+$_['text_register']      = 'Регистрация';
+$_['text_login']         = 'Авторизация';
+$_['text_order']         = 'История заказа';
+$_['text_transaction']   = 'Операции';
+$_['text_download']      = 'Файлы для скачивания';
+$_['text_logout']        = 'Выход';
+$_['text_checkout']      = 'Оформление заказа';
+$_['text_search']        = 'Поиск';
+$_['text_all']           = 'Смотреть Все';
---
+++ upload/catalog/language/russian/information/contact.php
@@ -0,0 +1,36 @@
+<?php
+// *	@copyright	OCSHOP.CMS \ ocshop.net 2011 - 2015.
+// *	@demo	http://ocshop.net
+// *	@blog	http://ocshop.info
+// *	@forum	http://forum.ocshop.info
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+// Heading
+$_['heading_title']  = 'Связаться с нами';
+
+// Text
+$_['text_location']  = 'Наше местонахождение';
+$_['text_store']     = 'Наши магазины';
+$_['text_contact']   = 'Написать нам';
+$_['text_address']   = 'Адрес';
+$_['text_telephone'] = 'Телефон';
+$_['text_fax']       = 'Факс';
+$_['text_open']      = 'Режим работы';
+$_['text_comment']   = 'Дополнительная информация';
+$_['text_success']   = '<p>Ваш запрос был успешно отправлен администрации магазина!</p>';
+
+// Entry
+$_['entry_name']     = 'Ваше имя';
+$_['entry_email']    = 'E-Mail адрес';
+$_['entry_enquiry']  = 'Ваше сообщение';
+$_['entry_captcha']  = 'Введите код, указанный на картинке';
+
+// Email
+$_['email_subject']  = 'Сообщение от %s';
+
+// Errors
+$_['error_name']     = 'Имя должно содержать от 3 до 32 символов!';
+$_['error_email']    = 'E-Mail адрес введен неверно!';
+$_['error_enquiry']  = 'Длина текста должна содержать от 10 до 3000 символов!';
+$_['error_captcha']  = 'Неверно введен код с картинки!';
