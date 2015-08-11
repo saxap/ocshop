5b62bb093a3b74ba7a74bf3a4cb1e8aff76fd63d
ocshop <info@vilkov.info>
2015-08-11 22:21:37 +0300

    lang fix

--- upload/admin/language/russian/design/banner.php
+++ upload/admin/language/russian/design/banner.php
@@ -7,7 +7,7 @@
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 // Heading
-$_['heading_title']      = 'Банеры';
+$_['heading_title']      = 'Баннеры';
 
 // Text
 $_['text_success']       = 'Настройки успешно изменены!';
