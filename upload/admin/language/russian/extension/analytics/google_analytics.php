8dea97b45d36f5580018bab1e2ced2a1fd22a8bc
ocshop <info@vilkov.info>
2015-08-12 22:20:58 +0300

    дополнение перевода

--- upload/admin/language/russian/extension/analytics/google_analytics.php
+++ upload/admin/language/russian/extension/analytics/google_analytics.php
@@ -10,11 +10,12 @@ $_['heading_title']    = 'Google Analytics';
 
 // Text
 $_['text_analytics']   = 'Analytics';
-$_['text_code']        = 'Войдите в Ваш <a href="http://www.google.com/analytics/" target="_blank"><u>Google Analytics</u></a> и после настройки добавьте сюда выданный код.';
 $_['text_edit']        = 'Настройки Google Analytics';
+$_['text_signup']      = 'Войдите в Ваш <a href="http://www.google.com/analytics/" target="_blank"><u>Google Analytics</u></a> и после настройки добавьте сюда выданный код.';
 
 // Entry
 $_['entry_code']       = 'Код Google Analytics';
+$_['entry_status']     = 'Статус';
 
 // Error
 $_['error_permission'] = 'У вас недостаточно прав для внесения изменений!';
--- upload/admin/language/russian/extension/captcha/google_captcha.php
+++ upload/admin/language/russian/extension/captcha/google_captcha.php
@@ -9,11 +9,14 @@
 $_['heading_title']    = 'Google reCAPTCHA';
 
 // Text
+$_['text_captcha']     = 'Капча';
+$_['text_edit']        = 'Настройки Google reCAPTCHA';
 $_['text_signup']      = 'Перейдите <a href="https://www.google.com/recaptcha/intro/index.html" target="_blank"><u>Google reCAPTCHA</u></a> и зарегистрируйте свой магазин.';
 
 // Entry
 $_['entry_key']        = 'Ключ сайта';
 $_['entry_secret']     = 'Секретный ключ';
+$_['entry_status']     = 'Статус';
 
 // Error
 $_['error_permission'] = 'У вас недостаточно прав для внесения изменений!';
