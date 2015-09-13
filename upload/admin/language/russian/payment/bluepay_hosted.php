5f6077c3ea7e43cd7c76ca17790fe4ee98c7b7ce
ocshop <info@vilkov.info>
2015-09-13 07:27:35 +0300

    Дополнение перевода

--- upload/admin/language/russian/payment/bluepay_hosted.php
+++ upload/admin/language/russian/payment/bluepay_hosted.php
@@ -10,9 +10,9 @@
 $_['heading_title']            = 'BluePay Hosted Form';
 
 // Text
-$_['text_payment']             = 'Платеж';
-$_['text_success']             = 'Настройки успешно изменены!';
-$_['text_edit']                = 'Редактирование';
+$_['text_payment']             = 'Payment';
+$_['text_success']             = 'Success: You have modified BluePay Hosted Form account details!';
+$_['text_edit']                = 'Edit BluePay Hosted Form';
 $_['text_bluepay_hosted'] = '<a href="http://www.bluepay.com/preferred-partner/opencart" target="_blank"><img src="view/image/payment/bluepay.jpg" alt="BluePay Hosted Form" title="BluePay Hosted Form" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_test']                = 'Test';
 $_['text_live']                = 'Live';
@@ -23,34 +23,34 @@ $_['text_release_ok_order']    = 'Release was successful';
 $_['text_rebate_ok']           = 'Rebate was successful';
 $_['text_rebate_ok_order']     = 'Rebate was successful, order status updated to rebated';
 $_['text_void_ok']             = 'Void was successful, order status updated to voided';
-$_['text_payment_info']        = 'Платёжные реквизиты';
+$_['text_payment_info']        = 'Payment information';
 $_['text_release_status']			= 'Payment released';
 $_['text_void_status']				= 'Payment voided';
 $_['text_rebate_status']			= 'Payment rebated';
 $_['text_order_ref']				= 'Order ref';
 $_['text_order_total']				= 'Total authorised';
 $_['text_total_released']			= 'Total released';
-$_['text_transactions']				= 'Операции';
-$_['text_column_amount']			= 'Итого';
-$_['text_column_type']				= 'Тип';
-$_['text_column_date_added']		= 'Дата добавления';
+$_['text_transactions']				= 'Transactions';
+$_['text_column_amount']			= 'Amount';
+$_['text_column_type']				= 'Type';
+$_['text_column_date_added']		= 'Date Added';
 $_['text_confirm_void']				= 'Are you sure you want to void the payment?';
 $_['text_confirm_release']			= 'Are you sure you want to release the payment?';
 $_['text_confirm_rebate']			= 'Are you sure you want to rebate the payment?';
 
 // Entry
-$_['entry_account_name']			= 'Название счета';
+$_['entry_account_name']			= 'Account Name';
 $_['entry_account_id']				= 'Account ID';
 $_['entry_secret_key']				= 'Secret Key';
 $_['entry_test']					= 'Test Mode';
 $_['entry_transaction']				= 'Transaction Method';
 $_['entry_card_amex']				= 'Amex';
 $_['entry_card_discover']			= 'Discover';
-$_['entry_total']					= 'Всего';
-$_['entry_order_status']			= 'Статус';
-$_['entry_geo_zone']				= 'Географическая зона';
-$_['entry_status']					= 'Статус';
-$_['entry_sort_order']				= 'Порядок сортировки';
+$_['entry_total']					= 'Total';
+$_['entry_order_status']			= 'Order Status';
+$_['entry_geo_zone']				= 'Geo Zone';
+$_['entry_status']					= 'Status';
+$_['entry_sort_order']				= 'Sort Order';
 $_['entry_debug']					= 'Debug logging';
 
 // Help
@@ -61,12 +61,12 @@ $_['help_cron_job_token']			= 'Make this long and hard to guess';
 $_['help_cron_job_url']				= 'Set a cron job to call this URL';
 
 // Button
-$_['btn_release']					= 'Release';
-$_['btn_rebate']					= 'Rebate / refund';
-$_['btn_void']						= 'Void';
+$_['button_release']				= 'Release';
+$_['button_rebate']					= 'Rebate / refund';
+$_['button_void']					= 'Void';
 
 // Error
-$_['error_permission']				= 'У вас недостаточно прав для внесения изменений!';
+$_['error_permission']				= 'Warning: You do not have permission to modify payment BluePay!';
 $_['error_account_name']			= 'Account Name Required!';
 $_['error_account_id']				= 'Account ID Required!';
 $_['error_secret_key']				= 'Secret Key Required!';
--- upload/admin/language/russian/payment/worldpay.php
+++ upload/admin/language/russian/payment/worldpay.php
@@ -10,7 +10,7 @@
 $_['heading_title']				 = 'Worldpay Online Payments';
 
 // Text
-$_['text_payment']				 = 'Платеж';
+$_['text_payment']				 = 'Payment';
 $_['text_success']				 = 'Success: You have modified Worldpay account details!';
 $_['text_worldpay']				 = '<a href="https://online.worldpay.com/signup/ee48b6e6-d3e3-42aa-a80e-cbee3f4f8b09" target="_blank"><img src="view/image/payment/worldpay.png" alt="Worldpay" title="Worldpay" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_test']					 = 'Test';
@@ -25,11 +25,11 @@ $_['text_void_ok']				 = 'Void was successful, order status updated to voided';
 // Entry
 $_['entry_service_key']			 = 'Service Key';
 $_['entry_client_key']			 = 'Client Key';
-$_['entry_total']				 = 'Всего';
-$_['entry_order_status']		 = 'Статус';
-$_['entry_geo_zone']			 = 'Географическая зона';
-$_['entry_status']				 = 'Статус';
-$_['entry_sort_order']			 = 'Порядок сортировки';
+$_['entry_total']				 = 'Total';
+$_['entry_order_status']		 = 'Order Status';
+$_['entry_geo_zone']			 = 'Geo Zone';
+$_['entry_status']				 = 'Status';
+$_['entry_sort_order']			 = 'Sort Order';
 $_['entry_debug']				 = 'Debug logging';
 $_['entry_card']				 = 'Store Cards';
 $_['entry_secret_token']		 = 'Secret Token';
@@ -58,26 +58,26 @@ $_['help_webhook_url']			 = 'Set Worldpay webhooks to call this URL';
 $_['help_cron_job_url']			 = 'Set a cron job to call this URL';
 
 // Tab
-$_['tab_settings']				 = 'Настройки';
-$_['tab_order_status']			 = 'Статус';
+$_['tab_settings']				 = 'Settings';
+$_['tab_order_status']			 = 'Order Status';
 
 // Error
-$_['error_permission']			 = 'У вас недостаточно прав для внесения изменений!';
+$_['error_permission']			 = 'Warning: You do not have permission to modify payment Worldpay!';
 $_['error_service_key']			 = 'Service Key Required!';
 $_['error_client_key']			 = 'Client Key Required!';
 
 // Order page - payment tab
-$_['text_payment_info']			 = 'Платёжные реквизиты';
+$_['text_payment_info']			 = 'Payment information';
 $_['text_refund_status']		 = 'Payment refund';
 $_['text_order_ref']			 = 'Order ref';
 $_['text_order_total']			 = 'Total authorised';
 $_['text_total_released']		 = 'Total released';
-$_['text_transactions']			 = 'Операции';
-$_['text_column_amount']		 = 'Итого';
-$_['text_column_type']			 = 'Тип';
-$_['text_column_date_added']	 = 'Дата добавления';
+$_['text_transactions']			 = 'Transactions';
+$_['text_column_amount']		 = 'Amount';
+$_['text_column_type']			 = 'Type';
+$_['text_column_date_added']	 = 'Added';
 
 $_['text_confirm_refund']		 = 'Are you sure you want to refund the payment?';
 
-$_['btn_refund']				 = 'Rebate / refund';
+$_['button_refund']				 = 'Rebate / refund';
 
