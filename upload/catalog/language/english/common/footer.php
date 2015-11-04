9453e505a4bd24b8a706d8875ee7d3a8178a5487
ocshop <info@vilkov.info>
2015-11-04 02:37:04 +0200

    opencart

--- upload/catalog/language/english/common/footer.php
+++ upload/catalog/language/english/common/footer.php
@@ -19,4 +19,4 @@ $_['text_account']      = 'My Account';
 $_['text_order']        = 'Order History';
 $_['text_wishlist']     = 'Wish List';
 $_['text_newsletter']   = 'Newsletter';
-$_['text_powered']      = 'Powered By <a href="http://forum.opencart.pro">OPENCART.PRO</a><br /> %s &copy; %s';
+$_['text_powered']      = 'Powered By <a href="http://opencart.pro/">OPENCART.PRO</a><br /> %s &copy; %s';
--- upload/catalog/language/russian/common/footer.php
+++ upload/catalog/language/russian/common/footer.php
@@ -19,4 +19,4 @@ $_['text_account']      = 'Личный кабинет';
 $_['text_order']        = 'История заказа';
 $_['text_wishlist']     = 'Закладки';
 $_['text_newsletter']   = 'Рассылка';
-$_['text_powered']      = 'Работает на <a href="http://forum.opencart.pro">OPENCART.PRO</a><br /> %s &copy; %s';
+$_['text_powered']      = 'Работает на <a href="http://opencart.pro/">OPENCART.PRO</a><br /> %s &copy; %s';
