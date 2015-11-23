3f1bb0f89b07c89be9d522549985152d9dbe40a5
ocshop <info@vilkov.info>
2015-11-23 20:32:52 +0200

    fix sitemap yandex

--- upload/admin/controller/feed/google_sitemap.php
+++ upload/admin/controller/feed/google_sitemap.php
@@ -30,6 +30,7 @@ class ControllerFeedGoogleSitemap extends Controller {
 
 		$data['entry_status'] = $this->language->get('entry_status');
 		$data['entry_data_feed'] = $this->language->get('entry_data_feed');
+		$data['entry_data_yandex_feed'] = $this->language->get('entry_data_yandex_feed');
 
 		$data['button_save'] = $this->language->get('button_save');
 		$data['button_cancel'] = $this->language->get('button_cancel');
@@ -70,6 +71,7 @@ class ControllerFeedGoogleSitemap extends Controller {
 		}
 
 		$data['data_feed'] = HTTP_CATALOG . 'index.php?route=feed/google_sitemap';
+		$data['data_yandex_feed'] = HTTP_CATALOG . 'index.php?route=feed/yandex_sitemap';
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
