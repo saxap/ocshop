4433e96538fec300cef2ea5eee592409d38359f9
ocshop <info@vilkov.info>
2015-12-15 15:33:22 +0200

    2.1.0.1.10

--- upload/admin/index.php
+++ upload/admin/index.php
@@ -5,7 +5,7 @@
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 // Version
-define('VERSION', '2.1.0.1.9');
+define('VERSION', '2.1.0.1.10');
 
 // Configuration
 if (is_file('config.php')) {
