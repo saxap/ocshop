d4b501348f5957ff1fcc4bb44c98dbcc987eb307
ocshop <info@vilkov.info>
2016-01-16 20:17:13 +0200

    Fixed low risk XSS issue with user account address edit.
    Fixed low risk XSS issue with user account address edit. Backported fix
    to 2.1.0.1 -> new version 2.1.0.2

--- upload/admin/index.php
+++ upload/admin/index.php
@@ -5,7 +5,7 @@
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 // Version
-define('VERSION', '2.1.0.1.10');
+define('VERSION', '2.1.0.2');
 
 // Configuration
 if (is_file('config.php')) {
